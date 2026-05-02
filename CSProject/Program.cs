0c1716d90c7306a4d7f06051c7a8dc014c145a44
Rick Sun <yatyricky@gmail.com>
2026-05-03 01:22:31 +0800

    port to sharpforge and interop successfully

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -1,10 +1,66 @@
+using SFLib;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        BJDebugMsg("Hello SharpForge");
-        var unit = CreateUnit(Player(0), FourCC("hfoo"), 0, 0, 0);
-        BJDebugMsg(GetUnitName(unit));
-        BJDebugMsg(GetPlayerName(GetOwningPlayer(unit)));
+        LuaInterop.SetGlobal("CLI", LuaInterop.CreateTable());
+        
+        var FrameTimer = LuaInterop.Require("Lib.FrameTimer");
+        var Time = LuaInterop.Require("Lib.Time");
+        LuaInterop.Require("Lib.CoroutineExt");
+        LuaInterop.Require("Lib.TableExt");
+        LuaInterop.Require("Lib.StringExt");
+        LuaInterop.Require("Lib.native");
+
+        var systems = new List<LuaObject>();
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.ItemSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.SpellSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.BuffSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.DamageSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.ProjectileSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.ManagedAISystem"), "new"));
+        
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.InitAbilitiesSystem"), "new"));
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.BuffDisplaySystem"), "new"));
+
+#if MAP_NAME_echoisles || MAP_NAME_turtlerock || MAP_NAME_twistedmeadows
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.MeleeGameSystem"), "new"));
+#endif
+
+#if MAP_NAME_moonglade
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.MoonGladeSystem"), "new"));
+#endif
+
+#if MAP_NAME_twistedmeadows
+        systems.Add(LuaInterop.Call<LuaObject>(LuaInterop.Require("System.TwistedMeadowsSystem"), "new"));
+#endif
+
+        foreach (var system in systems)
+        {
+            LuaInterop.CallMethod(system, "Awake");
+        }
+
+        var group = CreateGroup();
+        GroupEnumUnitsInRect(group, bj_mapInitialPlayableArea, Filter(() =>
+        {
+            LuaInterop.CallGlobal("ExTriggerRegisterNewUnitExec", GetFilterUnit());
+        }));
+        DestroyGroup(group);
+
+        foreach (var system in systems)
+        {
+            LuaInterop.CallMethod(system, "OnEnable");
+        }
+
+        var game = LuaInterop.Call<LuaObject>(FrameTimer, "new", (float dt) =>
+        {
+            var now = MathRound(LuaInterop.Get<float>(Time, "Time") * 100) * 0.01f;
+            foreach (var system in systems)
+            {
+                LuaInterop.CallMethod(system, "Update", dt, now);
+            }
+        }, 1, -1);
+        LuaInterop.CallMethod(game, "Start");
     }
 }
--- LuaProject/Main.lua
+++ LuaProject/Main.lua
@@ -1,66 +1,109 @@
-local FrameTimer = require("Lib.FrameTimer")
-local Time = require("Lib.Time")
-require("Lib.CoroutineExt")
-require("Lib.TableExt")
-require("Lib.StringExt")
-require("Lib.native")
-
-local ipairs = ipairs
-
--- main logic
-
--- game machine
-
----@type SystemBase[]
-local systems = {
-    require("System.ItemSystem").new(),
-    require("System.SpellSystem").new(),
-    require("System.BuffSystem").new(),
-    require("System.DamageSystem").new(),
-    require("System.ProjectileSystem").new(),
-    require("System.ManagedAISystem").new(),
-
-    require("System.InitAbilitiesSystem").new(),
-    require("System.BuffDisplaySystem").new(),
-}
-
---#IF MAP_NAME_echoisles OR MAP_NAME_turtlerock OR MAP_NAME_twistedmeadows THEN
-table.insert(systems, require("System.MeleeGameSystem").new())
---#END
-
---#IF MAP_NAME_moonglade THEN
-table.insert(systems, require("System.MoonGladeSystem").new())
---#END
---#IF MAP_NAME_twistedmeadows THEN
-table.insert(systems, require("System.TwistedMeadowsSystem").new())
---#END
-
-for _, system in ipairs(systems) do
-    system:Awake()
+SF__ = SF__ or {}
+function SF__.ListNew__(items)
+    return { items = items or {}, version = 0 }
 end
 
-local group = CreateGroup()
-GroupEnumUnitsInRect(group, bj_mapInitialPlayableArea, Filter(function()
-    local s, m = pcall(ExTriggerRegisterNewUnitExec, GetFilterUnit())
-    if not s then
-        print(m)
-    end
-end))
-DestroyGroup(group)
-group = nil
-
-for _, system in ipairs(systems) do
-    system:OnEnable()
+function SF__.ListCount__(list)
+    return #list.items
 end
 
-local MathRound = MathRound
+function SF__.ListGet__(list, index)
+    return list.items[index + 1]
+end
 
-local game = FrameTimer.new(function(dt)
-    local now = MathRound(Time.Time * 100) * 0.01
-    for _, system in ipairs(systems) do
-        system:Update(dt, now)
+function SF__.ListSet__(list, index, value)
+    list.items[index + 1] = value
+    list.version = list.version + 1
+end
+
+function SF__.ListAdd__(list, value)
+    table.insert(list.items, value)
+    list.version = list.version + 1
+end
+
+function SF__.ListIterate__(list)
+    local version = list.version
+    local i = 0
+    return function()
+        if list.version ~= version then error("collection was modified during iteration") end
+        i = i + 1
+        local value = list.items[i]
+        if value ~= nil then return i, value end
     end
-end, 1, -1)
-game:Start()
+end
 
-require("sf")
+function SF__.ListSort__(list, less)
+    local compare = less or function(a, b) return a < b end
+    local items = list.items
+    for i = 2, #items do
+        local value = items[i]
+        local j = i - 1
+        while j >= 1 and compare(value, items[j]) do
+            items[j + 1] = items[j]
+            j = j - 1
+        end
+        items[j + 1] = value
+    end
+    list.version = list.version + 1
+    return list
+end
+
+-- Program
+SF__.Program = SF__.Program or {}
+function SF__.Program.Main(args)
+    CLI = {}
+    local FrameTimer = require("Lib.FrameTimer")
+    local Time = require("Lib.Time")
+    require("Lib.CoroutineExt")
+    require("Lib.TableExt")
+    require("Lib.StringExt")
+    require("Lib.native")
+    local systems = SF__.ListNew__({})
+    SF__.ListAdd__(systems, require("System.ItemSystem").new())
+    SF__.ListAdd__(systems, require("System.SpellSystem").new())
+    SF__.ListAdd__(systems, require("System.BuffSystem").new())
+    SF__.ListAdd__(systems, require("System.DamageSystem").new())
+    SF__.ListAdd__(systems, require("System.ProjectileSystem").new())
+    SF__.ListAdd__(systems, require("System.ManagedAISystem").new())
+    SF__.ListAdd__(systems, require("System.InitAbilitiesSystem").new())
+    SF__.ListAdd__(systems, require("System.BuffDisplaySystem").new())
+    do
+        local collection = systems
+        for i, system in SF__.ListIterate__(collection) do
+            system:Awake()
+        end
+    end
+    local group = CreateGroup()
+    GroupEnumUnitsInRect(group, bj_mapInitialPlayableArea, Filter(function()
+        ExTriggerRegisterNewUnitExec(GetFilterUnit())
+    end))
+    DestroyGroup(group)
+    do
+        local collection1 = systems
+        for i2, system1 in SF__.ListIterate__(collection1) do
+            system1:OnEnable()
+        end
+    end
+    local game = FrameTimer.new(function(dt)
+        local now = (MathRound((Time.Time * 100)) * 0.009999999776482582)
+        do
+            local collection3 = systems
+            for i4, system2 in SF__.ListIterate__(collection3) do
+                system2:Update(dt, now)
+            end
+        end
+    end, 1, (-1))
+    game:Start()
+end
+
+function SF__.Program.__Init(self)
+    self.__sf_type = SF__.Program
+end
+
+function SF__.Program.New()
+    local self = setmetatable({}, { __index = SF__.Program })
+    SF__.Program.__Init(self)
+    return self
+end
+
+SF__.Program.Main()
